6971fb7036abcdb61abdb605c5b824a5e95f5b98
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-06-09 17:00:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+
+    <form action="saluto.php" method="get">
+        <label for="nominativo">Nome:</label>
+        <input type="text" id="nominativo" name="nominativo">
+        <button type="submit">Saluta</button>
+    </form>
+</body>
+</html>
